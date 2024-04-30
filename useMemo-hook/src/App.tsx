f71ff71e9d5bb4b68580c36d2ef7549d6e324a64
dmtrth25 <vodolazhchenko25@gmail.com>
2024-05-01 00:57:52 +0300

    useMemo + ts

---
+++ useMemo-hook/src/App.tsx
@@ -0,0 +1,13 @@
+import Counter from './components/Counter'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  )
+}
+
+export default App
+
